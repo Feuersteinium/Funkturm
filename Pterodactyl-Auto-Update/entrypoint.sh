ee763b8286297e3385c8a0647670d4b63668044b
Feuersteinium <contact@feuerstein.dev>
2023-08-15 10:37:25 +0200

    Entryoint.sh will now download config file if necessary

--- Pterodactyl-Auto-Update/entrypoint.sh
+++ Pterodactyl-Auto-Update/entrypoint.sh
@@ -8,9 +8,21 @@ java -version
 
 
 # Downloading updater.py (Current Version 3.0)
-
 wget  https://github.com/Feuersteinium/mountain/releases/download/PAD-1.0/updater.py -Oupdater.py
 
+
+
+# Check for config
+
+if [ $(ls -l | grep "updater.toml" | wc -l)  -eq 1 ]
+then
+    echo "updater.toml does exist. Won't do anything."
+else
+    wget https://raw.githubusercontent.com/Feuersteinium/mountain/master/Pterodactyl-Auto-Update/updater.toml -Oupdater.toml
+fi
+
+
+echo "Ready for startup..."
 
 # RUN!
 python3 updater.py ; ${STARTUP}
