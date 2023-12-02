6f58f32cd4149127d73f5fe15047dfb071ddb86d
Feuersteinium <contact@feuerstein.dev>
2023-12-02 15:08:35 +0100

    The formatter will now generate one year :D

--- note-aggregation/monthly-formatter.py
+++ note-aggregation/monthly-formatter.py
@@ -5,16 +5,15 @@
 ## VARIABLES
 ## ------------------------------------------
 
-month = input("What month is it? \n >> " )
 year = input("What year is it? \n  >> ")
 monthd = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+monthm = ["Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember"]
 
 ## ------------------------------------------
 
 
 ## CONVERTER
 year = str(year)
-month = int(month)
 
 print("[CONVERSIONS] DONE!")
 
@@ -28,11 +27,6 @@ def is_leap_year():
 is_leap_year()
 
 ## EVERY MONTH-NUMBER LOWER THAN 10 WILL BE WRITTEN WITH AN 0 IN FRONT.
-if month < 10:
-    monthfinal = "0" + str(month)
-    print("[MONTH PLOTTER] Month " + str(month) + " is lower than 10! Added a decimal in front of " + str(month))
-else:
-    monthfinal = str(month)
 
 
 print("[MONTH PLOTTER] DONE!")
@@ -41,15 +35,36 @@ print("[OBSIDIAN LINK FORMATTER] Formatting link! Programm will add a few empty 
 ## THE FINAL PLOTTER
 ## THIS PROGRAMM WILL GENERATE THE OUTPUT, WHICH CAN BE COPYED IN OBSIDIAN LATER!
 
-for x in range(1,monthd[month - 1] + 1):
-    ## THIS WILL CHECK IF IT NEED TO ADD A 0
-    ## IF IT DOESNT, DAYFINAL WILL BE REWRITTEN TO STRING
-    if x < 10:
-        dayfinal = str(0) + str(x)
-    else: 
-        dayfinal = str(x)
+
+def yearmap():
+
+    f = open(str(year) + ".md", "a")
+    runner = 1
+
+    for element in monthd:
+        f.write("## " + monthm[runner -1] + "\n")
+
+        if runner < 10:
+            monthfinal = "0" + str(runner)
+        else:
+            monthfinal = str(runner)
+        runner += 1
+
+        # We cant start at 0 as usual, as the first day of the month would be day 0
+        for x in range(1,element +1):
+
+            ## THIS WILL CHECK IF IT NEED TO ADD A 0
+            ## IF IT DOESNT, DAYFINAL WILL BE REWRITTEN TO STRING
+            if x < 10:
+                dayfinal = str(0) + str(x)
+            else: 
+                dayfinal = str(x)
     
-    print("[[" + dayfinal + "-" + monthfinal + "-" + year + "]]")
+            f.write("[[" + dayfinal + "-" + monthfinal + "-" + year + "]]" + "\n")
+    f.close()
 
 
 print("\n \n \n \n \n \n \n[OBSIDIAN LINK FORMATTER] Everything done! Exiting...")
+
+
+yearmap()
