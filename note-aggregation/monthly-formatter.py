ff8bf83ff23273ab52e4d8f0b9d1d10b61889e72
Feuersteinium <contact@feuerstein.dev>
2023-12-03 14:25:50 +0100

    Monthly formatter will now recognize precisely

--- note-aggregation/monthly-formatter.py
+++ note-aggregation/monthly-formatter.py
@@ -1,56 +1,53 @@
 ## MONTHLY FILE GEN
 ## DATE PLOTTER
 
+## Constants
 
-## VARIABLES
-## ------------------------------------------
-
-year = input("What year is it? \n  >> ")
-monthd = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 monthm = ["Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember"]
 
-## ------------------------------------------
+## Functions
+
+def input_year():
+    try:
+        return(int(input("What year is it? \n  >> ")))
+    except ValueError:
+        print("The given year isnt a valid integer, exiting...")
+        exit()
+
+## This function takes a year as an input and will output a list with the lenght of each month in days. Important for leap years
+
+def generate_month_lenght(year):
+    monthd = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    if (year % 4 == 0 and year % 100 !=0) or year % 400 == 0:
+               monthd[1] = 29
+    return(monthd)
+
+## Parameters
+## year = year
+## mlist == month list (List with the lenght of each month)
 
 
-## CONVERTER
-year = str(year)
+def yearmap(year, mlist):
 
-print("[CONVERSIONS] DONE!")
-
-
-
-def is_leap_year():
-    if int(year) % 4 == 0:
-        print("Year is a leap year. Continiuing with a 29 day February :D")
-        monthd[1] = 29
-
-is_leap_year()
-
-## EVERY MONTH-NUMBER LOWER THAN 10 WILL BE WRITTEN WITH AN 0 IN FRONT.
-
-
-print("[MONTH PLOTTER] DONE!")
-print("[OBSIDIAN LINK FORMATTER] Formatting link! Programm will add a few empty lines for better copying \n \n \n \n \n \n \n")
-
-## THE FINAL PLOTTER
-## THIS PROGRAMM WILL GENERATE THE OUTPUT, WHICH CAN BE COPYED IN OBSIDIAN LATER!
-
-
-def yearmap():
-
+    # Open markdown file with the name of the year
     f = open(str(year) + ".md", "a")
+
+    # Setting runner variable to 1 (representing January)
     runner = 1
 
-    for element in monthd:
+    # Looping through each item of the month list, in order to generate a Obsidian style link for every day.
+    for element in mlist:
+        # Heading
         f.write("## " + monthm[runner -1] + "\n")
 
+        # 0 in front of the digit?
         if runner < 10:
             monthfinal = "0" + str(runner)
         else:
             monthfinal = str(runner)
         runner += 1
 
-        # We cant start at 0 as usual, as the first day of the month would be day 0
+        # We can't start at 0 as usual, as the first day of the month would be day 0.(starting at 1 and ending at n+1 instead of starting at 0 and ending at n.)
         for x in range(1,element +1):
 
             ## THIS WILL CHECK IF IT NEED TO ADD A 0
@@ -60,11 +57,11 @@ def yearmap():
             else: 
                 dayfinal = str(x)
     
-            f.write("[[" + dayfinal + "-" + monthfinal + "-" + year + "]]" + "\n")
+            f.write("[[" + dayfinal + "-" + monthfinal + "-" + str(year) + "]]" + "\n")
     f.close()
 
 
-print("\n \n \n \n \n \n \n[OBSIDIAN LINK FORMATTER] Everything done! Exiting...")
+## Run everything :D
 
-
-yearmap()
+yr = input_year()
+yearmap(yr, generate_month_lenght(yr))
